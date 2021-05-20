c7b0508d1ae8fa5bfa2c08a48ddeaa4308247f71
101603439 <101603439@student.swin.edu.au>
2021-05-20 12:35:52 +1000

    Only Finished Arrays

---
+++ Lists/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var numbers = new List<int>(){ 1, 2, 3, 4, 5 };
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Arrays_and_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //Names
+                string[] names = new string[5];
+                names[0] = "Steven";
+                names[1] = "Kevin";
+                names[2] = "Linda";
+                names[3] = "John";
+                names[4] = "Jane";
+
+                foreach(string s in names)
+                Console.WriteLine("Hello " + s);
+            }
+            //Number
+            int[] numbers = {34, 5, 67, 1, 99, 34, 44, 78, 34, 0};
+            int sum=0;
+            Console.WriteLine();
+            for(int counter=0; counter<numbers.Length;counter++) {
+                sum+=numbers[counter];
+            }
+            Console.WriteLine("The total is " + sum);
+
+        }
+    }
+}
